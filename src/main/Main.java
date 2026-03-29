294d2f703e527d5746a20297ab457a76c73b2232
neekhilsingh <sneekhilkumar@gmail.com>
2026-03-29 21:39:11 +0530

    add things in gitignore

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import model.*;
 import service.*;
 import dao.ParkingDAO;
 
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        // Fix ₹ and other Unicode symbols on Windows console
-        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+
 
         List<ParkingSlot> slots = new ArrayList<>();
         slots.add(new ParkingSlot(1, "CAR"));
@@ -37,7 +35,7 @@ public class Main {
 
         if (ticket != null) {
             double bill = billingService.calculateBill(ticket);
-            System.out.println("Total Bill: ₹" + bill);
+            System.out.println("Total Bill: Rs. " + bill);
         }
     }
 }
